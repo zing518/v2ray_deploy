3a1598b3634a5ced0ac60c700f4396f1880debbf
wangyangyangisme <626562203@qq.com>
2020-09-27 01:36:14 +0800

    fix

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ OK="${Green}[OK]${Font}"
 Error="${Red}[错误]${Font}"
 
 # 版本
-shell_version="1.1.6.7"
+shell_version="1.1.6.8"
 shell_mode="None"
 github_branch="master"
 version_cmp="/tmp/version_cmp.tmp"
